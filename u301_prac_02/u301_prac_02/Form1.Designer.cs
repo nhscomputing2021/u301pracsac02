0393057d1031054fc2d24b5fcef7d755f6d0fffd
nhscomputing2021 <81550918+nhscomputing2021@users.noreply.github.com>
2022-03-17 11:51:06 +1100

    Add files via upload

---
+++ u301_prac_02/u301_prac_02/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace u301_prac_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.category = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.purchaser = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.purchasePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.seller = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.profit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(134, 35);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(129, 47);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Choose a file";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.name,
+            this.category,
+            this.purchaser,
+            this.purchasePrice,
+            this.seller,
+            this.salePrice,
+            this.profit});
+            this.dataGridView1.Location = new System.Drawing.Point(31, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1241, 385);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Name";
+            this.name.Name = "name";
+            this.name.ReadOnly = true;
+            // 
+            // category
+            // 
+            this.category.HeaderText = "Category";
+            this.category.Name = "category";
+            this.category.ReadOnly = true;
+            // 
+            // purchaser
+            // 
+            this.purchaser.HeaderText = "Purchaser";
+            this.purchaser.Name = "purchaser";
+            this.purchaser.ReadOnly = true;
+            // 
+            // purchasePrice
+            // 
+            this.purchasePrice.HeaderText = "Purchase Price";
+            this.purchasePrice.Name = "purchasePrice";
+            this.purchasePrice.ReadOnly = true;
+            // 
+            // seller
+            // 
+            this.seller.HeaderText = "Seller";
+            this.seller.Name = "seller";
+            this.seller.ReadOnly = true;
+            // 
+            // salePrice
+            // 
+            this.salePrice.HeaderText = "Sale Price";
+            this.salePrice.Name = "salePrice";
+            this.salePrice.ReadOnly = true;
+            // 
+            // profit
+            // 
+            this.profit.HeaderText = "Profit";
+            this.profit.Name = "profit";
+            this.profit.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 575);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn category;
+        private System.Windows.Forms.DataGridViewTextBoxColumn purchaser;
+        private System.Windows.Forms.DataGridViewTextBoxColumn purchasePrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn seller;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salePrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn profit;
+    }
+}
+
---
+++ u301_prac_02/u301_prac_02/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace u301_prac_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                List<string> lines = new List<string>();
+                lines = File.ReadAllLines(filePath).ToList();
+
+                float total = 0f; ;
+                
+                foreach (string line in lines)
+                {
+                    
+                    List<string> fields = line.Split(',').ToList();
+                    float profit;
+                    //store sale price and purchased price from each line in the file into variables
+                    string salePrice = fields[5];
+                    float purchasedPrice = float.Parse(fields[3]);
+                    
+                    //calculate the profit
+                    if (float.TryParse(salePrice, out float _saleprice))
+                    {
+                        profit = _saleprice - purchasedPrice;
+                    }
+                    else
+                    {
+                        profit = purchasedPrice * -1;
+                    }
+                    // add the profit to the total
+                    total += profit;
+                    //append the profit field into fields so it is ready to populate the datagridview
+                    fields.Add(profit.ToString());
+                    
+                    dataGridView1.Rows.Add(fields.ToArray());
+                    
+                    
+                }
+                //create an empty row
+                dataGridView1.Rows.Add();
+                //grab the length of the rows in the datagridview
+                int rowCount = dataGridView1.Rows.Count;
+                //initialise value of the second last cell with text Total
+                dataGridView1.Rows[rowCount-1].Cells[5].Value = "Total";
+                //initialise value of the last cell with the value of the total
+                dataGridView1.Rows[rowCount-1].Cells[6].Value = $"${total.ToString()}";
+
+
+
+
+
+            }
+           
+            
+
+
+        }
+        
+
+        
+    }
+}
